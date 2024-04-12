dfbe60123b9c59daecc1fb5135dcdfdb0ae507db
naimishm <naimishm@hyperlinkinfosystem.net.in>
2024-04-12 13:03:46 +0530

    Splash screen added

---
+++ app/src/main/java/com/messageapp/SplashActivity.kt
@@ -0,0 +1,79 @@
+package com.messageapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.messageapp.ui.theme.MessageAppGreenTheme
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MessageAppGreenTheme {
+                SplashScreen()
+            }
+        }
+
+    }
+
+
+    @Preview
+    @Composable
+    private fun SplashScreen() {
+        LaunchedEffect(key1 = true) {
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(id = R.color.splash_green)),
+            contentAlignment = Alignment.Center,
+        ) {
+
+            Column(modifier = Modifier.wrapContentSize()) {
+                Image(
+                    painter = painterResource(id = R.drawable.splash_messages_icon),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.CenterHorizontally),
+                    contentDescription = stringResource(R.string.image_description_this_is_the_splash_screen_icon)
+                )
+
+                Text(
+                    text = stringResource(R.string.text_message),
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    maxLines = 1
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/messageapp/ui/theme/Theme.kt
+++ app/src/main/java/com/messageapp/ui/theme/Theme.kt
@@ -22,9 +22,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = green,
+    secondary = white,
+    tertiary = white
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,6 +37,27 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val DarkColorGreenScheme = darkColorScheme(
+    primary = green,
+    secondary = green,
+    tertiary = green
+)
+
+private val LightColorGreenScheme = lightColorScheme(
+    primary = green,
+    secondary = green,
+    tertiary = green
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
 @Composable
 fun MessageAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -52,6 +73,39 @@ fun MessageAppTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+
+@Composable
+fun MessageAppGreenTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorGreenScheme
+        else -> LightColorGreenScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
